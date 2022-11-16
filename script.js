86d4137aae1432fa05454bd8847c2e75811bc573
Darksiend <logosmk2@gmail.com>
2022-11-16 10:57:58 +0200

    Work

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ document.getElementById("form").addEventListener("submit", () => {
   getTaskFromInput();
 });
 
+document.querySelectorAll("input[type ='text']").forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value[0] != input.value[0].toUpperCase()) {
+      (input.value = input.value.slice(0, 1).toUpperCase()) +
+        input.value.slice(1, input.value.length);
+    }
+    if (input.value[0] === " ") {
+      input.value = "";
+    }
+  });
+});
+
+document.getElementById("input-start-todo").addEventListener("change", () => {
+  document.getElementById("input-end-todo").min =
+    document.getElementById("input-start-todo").value;
+});
+
 function getTaskFromInput() {
   let taskName = document.getElementById("name-of-task-input").value;
   let taskDescription = document.getElementById(
     "description-of-task-input"
   ).value;
   let taskStartDateTime = document.getElementById("input-start-todo").value;
-  console.log(taskStartDateTime);
   let taskEndDateTime = document.getElementById("input-end-todo").value;
   console.log(taskEndDateTime);
-  obj = {
+  let obj = {
     taskName: taskName,
     taskDescription: taskDescription,
     taskStartDateTime: taskStartDateTime,
@@ -22,3 +38,9 @@ function getTaskFromInput() {
 
   console.log(JSON.stringify(obj, null, 2));
 }
+
+function putInLocalStorage(obj) {}
+function toggleIsDone() {}
+function updateTask() {}
+function deleteTask() {}
+function taskInLocalStorage() {}
